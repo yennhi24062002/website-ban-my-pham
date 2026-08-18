a95800a8fa1b91f251114c94fdf9647d4a7a9917
yennhi24062002 <yennhi2462002@gmail.com>
2026-08-18 19:45:29 +0700

    Update API URL for Vercel to active Serveo endpoint

--- client/src/config/api.js
+++ client/src/config/api.js
@@ -1,2 +1,2 @@
-const API_BASE = "https://d0153a175775671f-42-116-13-128.serveousercontent.com/api";
+const API_BASE = process.env.REACT_APP_API_BASE_URL || "https://3bbecfac95c54b62-42-116-13-128.serveousercontent.com/api";
 export default API_BASE;
