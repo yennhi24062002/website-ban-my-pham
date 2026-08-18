3bdc85d948bc3011f2f5f4c5767a8f0a90f93ef0
yennhi24062002 <yennhi2462002@gmail.com>
2026-08-19 01:11:54 +0700

    Support PATCH, POST, PUT for nhap-hang route in admin.route.js

--- server/routes/admin.route.js
+++ server/routes/admin.route.js
@@ -3,5 +3,8 @@ const AdminController = require("../controller/admin.controller");
 
 router.get("/statistics", AdminController.getStatistics);
 router.patch("/nhap-hang/:maluachon", AdminController.nhapHang);
+router.post("/nhap-hang/:maluachon", AdminController.nhapHang);
+router.put("/nhap-hang/:maluachon", AdminController.nhapHang);
 
 module.exports = router;
+
